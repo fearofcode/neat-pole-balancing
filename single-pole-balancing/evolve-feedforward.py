33ad8172de17c5541406a6cc7c0a2ae204c8b192
Warren Henning <warren.henning@gmail.com>
2017-03-18 17:49:55 -0700

    Better fitness, ignore checkpoint, movie files, models

--- single-pole-balancing/evolve-feedforward.py
+++ single-pole-balancing/evolve-feedforward.py
@@ -4,7 +4,6 @@ Single-pole balancing experiment using a feed-forward neural network.
 
 from __future__ import print_function
 
-import math
 import os
 import pickle
 
@@ -14,10 +13,7 @@ import neat
 import visualize
 
 runs_per_net = 5
-simulation_seconds = 60.0
-
-"""
-simulation_steps = 1000
+simulation_steps = int(60.0/cart_pole.CartPole.time_step)
 
 
 # Use the NN network phenotype and the discrete actuator force function.
@@ -26,17 +22,15 @@ def eval_genome(genome, config):
 
     fitnesses = []
 
-    # prevent divide by zero
     eps = 0.00000001
 
     for runs in range(runs_per_net):
         sim = cart_pole.CartPole()
 
         # Run the given simulation for up to num_steps time steps.
-        steps = 0
-
-        # see section 3.5 of the NEAT paper
         f2_fitnesses = []
+
+        steps = 0
         while steps < simulation_steps:
             inputs = sim.get_scaled_state()
             action = net.activate(inputs)
@@ -51,49 +45,22 @@ def eval_genome(genome, config):
             if abs(sim.x) >= sim.position_limit or abs(sim.theta) >= sim.angle_limit_radians:
                 break
 
+            f2_fitnesses.append(0.75/(abs(sim.x) + abs(sim.theta) + eps))
             steps += 1
-            f2_fitnesses.append(0.75/(abs(sim.x) + abs(sim.dx) + abs(sim.theta) + abs(sim.dtheta) + eps))
 
-        # gruau fitness
+        # modified gruau fitness: don't add f2 fitnesses unless stable for the entire time
+
         f1 = float(steps) / simulation_steps
         f2 = 0.0
 
-        if steps >= 100:
+        if steps == simulation_steps:
             final_fitnesses = f2_fitnesses[-100:]
             f2 = sum(final_fitnesses)
 
         fitness = 0.1*f1 + 0.9*f2
 
         fitnesses.append(fitness)
-"""
 
-
-# Use the NN network phenotype and the discrete actuator force function.
-def eval_genome(genome, config):
-    net = neat.nn.FeedForwardNetwork.create(genome, config)
-
-    fitnesses = []
-
-    for runs in range(runs_per_net):
-        sim = cart_pole.CartPole()
-
-        # Run the given simulation for up to num_steps time steps.
-        fitness = 0.0
-        while sim.t < simulation_seconds:
-            inputs = sim.get_scaled_state()
-            action = net.activate(inputs)
-
-            # Apply action to the simulated cart-pole
-            force = cart_pole.discrete_actuator_force(action)
-            sim.step(force)
-
-            # Stop if the network fails to keep the cart within the position or angle limits.
-            # The per-run fitness is the number of time steps the network can balance the pole
-            # without exceeding these limits.
-            if abs(sim.x) >= sim.position_limit or abs(sim.theta) >= sim.angle_limit_radians:
-                break
-
-            fitness = sim.t
     # The genome's fitness is its worst performance across all runs.
     return min(fitnesses)
 
@@ -128,7 +95,6 @@ def run():
 
     print(winner)
 
-    """
     visualize.plot_stats(stats, ylog=True, view=True, filename="feedforward-fitness.svg")
     visualize.plot_species(stats, view=True, filename="feedforward-speciation.svg")
 
@@ -141,7 +107,6 @@ def run():
                        filename="winner-feedforward-enabled.gv", show_disabled=False)
     visualize.draw_net(config, winner, view=True, node_names=node_names,
                        filename="winner-feedforward-enabled-pruned.gv", show_disabled=False, prune_unused=True)
-    """
 
 if __name__ == '__main__':
     run()
